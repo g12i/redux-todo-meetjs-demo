b37d5a4853cdf65b90f16af3eb8f66e2534ddc46
g12i <grzebieniowski@gmail.com>
2017-05-30 19:19:58 +0200

    Init React-Redux provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import Application from './Application';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 import './index.css';
 
-ReactDOM.render(
-  <Application />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Provider store={store}>
+    <Application />
+  </Provider>
+), document.getElementById('root'));
 
 registerServiceWorker();
