98c4379cbfe268a821894f41eca83f2c2821afba
g12i <grzebieniowski@gmail.com>
2017-05-30 19:00:23 +0200

    Dummy app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './Application';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<Application />, document.getElementById('root'));
+registerServiceWorker();
