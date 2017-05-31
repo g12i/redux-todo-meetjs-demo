719d4f54db7a55faa0d7ac26eeaa5f01a477f57f
g12i <grzebieniowski@gmail.com>
2017-05-31 19:26:20 +0200

    Make showcase item a link

--- src/components/ListShowcaseItem/index.js
+++ src/components/ListShowcaseItem/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-const ListShowcaseItem = ({ title }) => (
-  <div className="ListShowcaseItem">
+const ListShowcaseItem = ({ title, listId, onClick }) => (
+  <a className="ListShowcaseItem" onClick={onClick}>
     <div className="ListShowcaseItem__Content">
       <h3>{title}</h3>
+      <div>
+        <label style={{ display: 'block' }}><input type="checkbox" /> Elo</label>
+        <label style={{ display: 'block' }}><input type="checkbox" /> Elo</label>
+        <label style={{ display: 'block' }}><input type="checkbox" /> Elo</label>
+        <label style={{ display: 'block' }}><input type="checkbox" /> Elo</label>
+        <label style={{ display: 'block' }}><input type="checkbox" /> Elo</label>
+      </div>
     </div>
-  </div>
+  </a>
 );
 
 ListShowcaseItem.propTypes = {
   title: PropTypes.string.isRequired,
   listId: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default ListShowcaseItem;
