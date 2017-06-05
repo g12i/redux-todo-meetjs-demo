8ed6103555acc6ab92cc2499adcfedb8620b6741
g12i <grzebieniowski@gmail.com>
2017-06-05 20:41:55 +0200

    Rename ListPreview to ListEditor

--- src/components/ListEditor/index.js
+++ src/components/ListEditor/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '../Button';
 import './style.css';
 
 
-const ListPreview = ({ title, id, todos, onChangeTitle, onClickTodo, onChangeTodo, onAddTodo }) => {
+const ListEditor = ({
+  title,
+  id,
+  todos,
+  onChangeListTitle,
+  onRemoveList,
+  onSaveList,
+  onChangeTodoCheckbox,
+  onChangeTodoContent,
+  onAddTodo,
+  onRemoveTodo,
+ }) => {
   const uncompleted = todos.filter(({ completed }) => !completed);
   const completed = todos.filter(({ completed }) => completed);
 
   const Todo = ({ completed, content, id }) => (
-    <div className={`ListPreview__Todo ListPreview__Todo--${(completed ? 'Completed' : 'UnCompleted')}`} key={id}>
-      <input className="ListPreview__TodoCheckbox" type="checkbox" checked={completed} onClick={() => onClickTodo(id)} />
-      <input className="ListPreview__TodoInput" type="text" value={content} onChange={e => onChangeTodo(id, e.target.value)} />
+    <div className={`ListEditor__Todo ListEditor__Todo--${(completed ? 'Completed' : 'UnCompleted')}`} key={id}>
+      <input className="ListEditor__TodoCheckbox" type="checkbox" checked={completed} onClick={() => onChangeTodoCheckbox(id)} />
+      <input className="ListEditor__TodoInput" type="text" value={content} onChange={e => onChangeTodoContent(id, e.target.value)} />
+      <button className="ListEditor__TodoDelete" onClick={() => onRemoveTodo(id)}>&times;</button>
     </div>
   );
 
@@ -21,22 +34,30 @@ const ListPreview = ({ title, id, todos, onChangeTitle, onClickTodo, onChangeTod
     onAddTodo(e.target.value);
   };
 
+  const AddTodo = (
+    <div className="ListEditor__Todo">
+      <span className="ListEditor__TodoNew">＋</span>
+      <input className="ListEditor__TodoInput" type="text" placeholder="Add new" onKeyDown={handleKeyDownOnNewTodo} />
+    </div>
+  );
+
   return (
-    <div className="ListPreview">
-      <input type="text" className="ListPreview__TitleInput" onChange={e => onChangeTitle(e.target.value)} value={title} />
+    <div className="ListEditor">
+      <input type="text" className="ListEditor__TitleInput" onChange={e => onChangeListTitle(e.target.value)} value={title} />
       <div>
-        <div className="ListPreview__Todo">
-          <span className="ListPreview__TodoNew">＋</span>
-          <input className="ListPreview__TodoInput" type="text" placeholder="Add new" onKeyDown={handleKeyDownOnNewTodo} />
-        </div>
+        {AddTodo}
         {uncompleted.map(Todo)}
         {completed.map(Todo)}
+      </div>
+      <div className="ListEditor__Actions">
+        <Button onClick={onRemoveList}>Remove</Button>
+        <Button theme="blue" onClick={onSaveList}>Save</Button>
       </div>
     </div>
   )
 };
 
-ListPreview.propTypes = {
+ListEditor.propTypes = {
   title: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   todos: PropTypes.arrayOf(PropTypes.shape({
@@ -44,10 +65,13 @@ ListPreview.propTypes = {
     completed: PropTypes.bool.isRequired,
     content: PropTypes.string.isRequired,
   })).isRequired,
-  onClickTodo: PropTypes.func.isRequired,
-  onChangeTitle: PropTypes.func.isRequired,
-  onChangeTodo: PropTypes.func.isRequired,
+  onChangeListTitle: PropTypes.func.isRequired,
+  onRemoveList: PropTypes.func.isRequired,
+  onSaveList: PropTypes.func.isRequired,
+  onChangeTodoCheckbox: PropTypes.func.isRequired,
+  onChangeTodoContent: PropTypes.func.isRequired,
   onAddTodo: PropTypes.func.isRequired,
+  onRemoveTodo: PropTypes.func.isRequired,
 };
 
-export default ListPreview;
+export default ListEditor;
