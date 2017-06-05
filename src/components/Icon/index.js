889593f7b0f66cb3523683884442f2213fc741e3
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2017-06-05 21:35:30 +0200

    Tweak Icons

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,2 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
 export { default as check } from './check.svg';
 export { default as cancel } from './cancel.svg';
+
+const Icon = ({ icon, alt }) => <span className="Icon"><img src={icon} alt={alt} /></span>
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
+
+export default Icon;
