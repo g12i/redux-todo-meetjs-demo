832cc7e497cc42675352f6e8e8e11085876d3c3d
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2017-06-06 19:36:48 +0200

    Remove UI reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux';
 
 import lists from './lists';
 import todos from './todos';
-import ui from './ui';
 
 export default combineReducers({
   lists,
   todos,
-  ui,
 });
