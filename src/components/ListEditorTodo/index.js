da5e380343743c700a57b81faedea1e3d4dc6fad
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2017-06-06 19:34:45 +0200

    Pretty the formatting

--- src/components/ListEditorTodo/index.js
+++ src/components/ListEditorTodo/index.js
@@ -5,13 +5,20 @@ import Button from '../Button';
 import Checkbox from '../Checkbox';
 import Icon, { minus } from '../Icon';
 
-const ListEditorTodo = ({ completed, content, id, onChangeTodoContent, onChangeTodoCompletionStatus, onClickRemoveTodo }) => (
-  <div className={`ListEditor__Todo ListEditor__Todo--${(completed ? 'Completed' : 'UnCompleted')}`} key={id}>
-    <Checkbox checked={completed} onChange={() => onChangeTodoCompletionStatus(id)} />
-    <input className="ListEditor__TodoInput" type="text" value={content} onChange={e => onChangeTodoContent(id, e.target.value)} />
-    <Button onClick={() => onClickRemoveTodo(id)}><Icon icon={minus} alt="-" /></Button>
-  </div>
-);
+const ListEditorTodo = ({
+  completed,
+  content,
+  id,
+  onChangeTodoContent,
+  onChangeTodoCompletionStatus,
+  onClickRemoveTodo
+   }) => (
+    <div className={`ListEditor__Todo ListEditor__Todo--${(completed ? 'Completed' : 'UnCompleted')}`} key={id}>
+      <Checkbox checked={completed} onChange={() => onChangeTodoCompletionStatus(id)} />
+      <input className="ListEditor__TodoInput" type="text" value={content} onChange={e => onChangeTodoContent(id, e.target.value)} />
+      <Button onClick={() => onClickRemoveTodo(id)}><Icon icon={minus} alt="-" /></Button>
+    </div>
+  );
 
 ListEditorTodo.propTypes = {
   completed: PropTypes.bool.isRequired,
