339ab7b0fc9b8e10c824723ad6e3b19b60cf26f4
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2017-06-06 19:09:33 +0200

    Add changeListTItle action to lists reducer

--- src/reducers/lists.js
+++ src/reducers/lists.js
@@ -2,6 +2,7 @@ import uuid from 'uuid';
 
 const ADD_LIST = 'lists/ADD';
 const REMOVE_LIST = 'lists/REMOVE';
+const CHANGE_LIST_TITLE = 'lists/CHANGE_TITLE';
 
 export const addList = title => ({
   type: ADD_LIST,
@@ -17,6 +18,11 @@ export const removeList = id => ({
   payload: id
 });
 
+export const changeListTitle = ({ id, title }) => ({
+  type: CHANGE_LIST_TITLE,
+  payload: { id, title }
+})
+
 export default function (state = [], action) {
   switch (action.type) {
     case ADD_LIST:
@@ -24,6 +30,9 @@ export default function (state = [], action) {
     case REMOVE_LIST:
       return state
         .filter(({ id }) => id !== action.payload);
+    case CHANGE_LIST_TITLE:
+      return state
+        .map(list => list.id === action.payload.id ? { ...list, title: action.payload.title } : list)
     default:
       return state;
   }
