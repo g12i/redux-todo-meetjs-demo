11fdeaaf62b339076756b09a72dc0b6037286c4d
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2017-06-07 21:58:05 +0200

    Make ListEditor a class component, as it needs ref

--- src/components/ListEditor/index.js
+++ src/components/ListEditor/index.js
@@ -1,58 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button';
 import './style.css';
 
 import ListEditorTodo from '../ListEditorTodo';
 
-const ListEditor = ({
+class ListEditor extends Component {
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    todos: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+      content: PropTypes.string.isRequired,
+      order: PropTypes.number.isRequired,
+    })).isRequired,
+    onChangeListTitle: PropTypes.func.isRequired,
+    onRemoveList: PropTypes.func.isRequired,
+    onAddTodo: PropTypes.func.isRequired,
+  }
+
+  render() {
+    const {
   title,
-  id,
   todos,
   onChangeListTitle,
   onRemoveList,
   onAddTodo,
-   }) => {
-  const uncompleted = todos.filter(({ completed }) => !completed);
-  const completed = todos.filter(({ completed }) => completed);
+    } = this.props;
+    const sorted = [...todos].sort((a, b) => b.order - a.order);
+    const uncompleted = sorted.filter(({ completed }) => !completed);
+    const completed = sorted.filter(({ completed }) => completed);
   const handleKeyDownOnNewTodo = e => {
     if (e.key === 'Enter') {
-      onAddTodo(e.target.value);
+        onAddTodo(e);
+        this.$input.value = '';
     }
   };
 
   const NewTodoInput = (
     <div className="ListEditor__Todo ListEditor__Todo--New">
-      <input className="ListEditor__TodoInput" type="text" placeholder="Add new" onKeyDown={handleKeyDownOnNewTodo} />
+        <input className="ListEditor__TodoInput" type="text" placeholder="Add new" onKeyDown={handleKeyDownOnNewTodo} ref={this.createRef('$input')} />
     </div>
   );
 
   return (
     <div className="ListEditor">
-      <input type="text" className="ListEditor__TitleInput" onChange={e => onChangeListTitle(e.target.value)} value={title} />
+        <input type="text" className="ListEditor__TitleInput" onChange={onChangeListTitle} value={title} />
       <div>
         {NewTodoInput}
         {uncompleted.map(todo => <ListEditorTodo key={todo.id} {...todo} />)}
         {completed.map(todo => <ListEditorTodo key={todo.id} {...todo} />)}
       </div>
       <div className="ListEditor__Actions">
-        <Button dark onClick={() => onRemoveList(id)}>Remove</Button>
-      </div>
+          <Button dark onClick={onRemoveList}>Remove</Button>
     </div>
+      </div >
   )
-};
+  }
 
-ListEditor.propTypes = {
-  title: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired,
-    content: PropTypes.string.isRequired,
-  })).isRequired,
-  onChangeListTitle: PropTypes.func.isRequired,
-  onRemoveList: PropTypes.func.isRequired,
-  onAddTodo: PropTypes.func.isRequired,
-};
+  createRef = key => ref => this[key] = ref;
+
+}
 
 export default ListEditor;
